1686c8a2434a6bd052a60421db5cd43a990be240
NickBulbeck <bulbecknick@gmail.com>
2019-11-24 10:38:03 +0000

    Added basic blank files with comments (no code yet)

---
+++ js/dataAccess.js
@@ -0,0 +1,9 @@
+//	dataAccess.js holds all the functions that access campaign-chart data.
+// It does NOT contain:
+//  - any business logic
+//  - any data processing [NTS: may have to define 'data-processing' carefully at some future point]
+//  - any actual data
+//
+// The data itself is - as at 23rd November 2019 - in data/data.js; this will change at some point and 
+// it'll go into a true database.
+
