c39de8bd6f192d4c8a8fdfc4753dcc5ded17493c
NickBulbeck <26549198+NickBulbeck@users.noreply.github.com>
2025-09-09 08:29:54 +0100

    Update standard chart

--- js/standardChart.js
+++ js/standardChart.js
@@ -10,5 +10,10 @@ const standardChartDescriptions = [
     [" - Check this month's chart and scribbles doc",[310,220],"top"],
     [" - Check kitchen calendar",[310,270],"top"],
     [" - Round of kettle-bell exercises",[310,320],"top"],
-    [" - 2 minutes recorded speaking",[310,370],"top"]
+    [" - 2 minutes recorded speaking",[310,370],"top"],
+    [" - Clear kitchen at lunchtime",[310,420],"top"],
+    ["Prayer -",[420,20],"munro"],
+    [" - Morning",[510,20],"top"],
+    [" - Midday",[510,70],"top"],
+    [" - Evening",[510,120],"top"]
 ]
